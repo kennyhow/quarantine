c4dc1376bc8ae487e18123392da212142d91c2cc
Kennyhow <kennethyonghw@gmail.com>
2020-04-26 16:27:17 +0800

    Copy related_programming.py

---
+++ first_actual_attempt/related_programming.py
@@ -0,0 +1,5 @@
+def programming_keywords():
+    return ['c++', 'java', 'programming', 'lisp', 'algorithm', 'recursion',
+    'python', 'perl', 'php', 'sql', 'kotlin', 'erlang', 'haskell']
+
+# returns a list of words that are commonly associated with programming
